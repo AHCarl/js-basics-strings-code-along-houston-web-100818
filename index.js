dc4fee6545238b63619c3342eb62527b015f9477
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 01:13:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = "Welcome to Flatbook, " + currentUser.capitalize + "!"
+const welcomeMessage = "Welcome to Flatbook, " + currentUser.toUpperCase() + "!"
 
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
